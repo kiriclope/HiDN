80831fd1b0f0b1d140933860f2eb52fbe064ce34
leon <kiriclope@gmail.com>
2026-03-06 17:08:31 +0100

    Added model Comparison to meta and single pca

--- notebooks/setup.py
+++ notebooks/setup.py
@@ -9,7 +9,7 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 print("Python exe")
 print(sys.executable)
 
-sns.set_context("poster")
+# sns.set_context("poster")
 sns.set_style("ticks")
 plt.rc("axes.spines", top=False, right=False)
 fig_path = '../figs/'
@@ -17,9 +17,9 @@ golden_ratio = (5**.5 - 1) / 2
 width = 6
 height = width * golden_ratio
 
-matplotlib.rcParams['figure.figsize'] = [width, height]
-matplotlib.rcParams['lines.markersize'] = 10
-matplotlib.rcParams['axes.labelsize'] = 20
-matplotlib.rcParams['axes.titlesize'] = 20
-matplotlib.rcParams['xtick.labelsize'] = 18
-matplotlib.rcParams['ytick.labelsize'] = 18
+# matplotlib.rcParams['figure.figsize'] = [width, height]
+# matplotlib.rcParams['lines.markersize'] = 10
+# matplotlib.rcParams['axes.labelsize'] = 20
+# matplotlib.rcParams['axes.titlesize'] = 20
+# matplotlib.rcParams['xtick.labelsize'] = 18
+# matplotlib.rcParams['ytick.labelsize'] = 18
